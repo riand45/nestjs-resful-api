9a5ad682178b5c657e54c453312af63be4742db4
Riand Pratama <riandpratama4570@gmail.com>
2021-06-08 22:14:31 +0700

    add middleware with jwt in controller

--- src/modules/locations/locations.controller.ts
+++ src/modules/locations/locations.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { LocationsService } from './locations.service';
 
 @Controller('locations')
 export class LocationsController {
   constructor(private locationService: LocationsService) { }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   async all() {
     return await this.locationService.findAll();
